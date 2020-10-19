5d63b708689f4a3e0e3a4c70520134798c702893
Vasily Kovnev <v.k95@yandex.ru>
2020-10-19 17:50:45 +0300

    task complete!

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (matrix === undefined || matrix.length == 0) return [];
+
+    return matrix.reduce((prev, cur, index) => {
+        return index % 2 == 0 ? prev.concat(cur) : prev.concat(cur.reverse());
+    });
+};
